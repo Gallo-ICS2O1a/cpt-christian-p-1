14d5827cec19733cae7e7220e7985bc7eaebedf3
CPatroiu <34140470+CPatroiu@users.noreply.github.com>
2018-01-09 14:07:36 -0500

    Update my-app.py

--- my-app.py
+++ my-app.py
@@ -7,6 +7,7 @@ asteroids_pos_y = 0
 asteroids_speed_x = 0
 asteroids_speed_y = 2
 asteroids_size = 35
+y = 0
 
 
 def setup():
@@ -21,6 +22,7 @@ def draw():
     global asteroids_speed_x
     global asteroids_speed_y
     global asteroids_size
+    global y
     background(0)
     
     #Player ball
@@ -30,8 +32,15 @@ def draw():
     #Player ball movement
     player_x = mouseX
     player_y = height - player_size/2
-
+    
+    #Asteroid
+    #Draw ball 1
+    noStroke()
+    fill(255, 255, 0)
+    ellipse(asteroids_pos_x, asteroids_pos_y, asteroids_size, asteroids_size)
+    
+    
     #Asteroids falling
+    asteroids_pos_y += asteroids_speed_y
+    
    
-        
-        
